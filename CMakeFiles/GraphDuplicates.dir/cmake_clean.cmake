0526594eea5f6549f1b5f19319ccb15ee849269e
Alejandro <alejandro@devop.one>
2022-04-27 13:23:10 +0100

    Initial files

---
+++ CMakeFiles/GraphDuplicates.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GraphDuplicates.dir/src/main.o"
+  "GraphDuplicates"
+  "GraphDuplicates.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GraphDuplicates.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
